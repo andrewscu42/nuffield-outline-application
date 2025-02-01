c7638415a55a42527c0247f9918398c81db5269d
sandleon <andrew.scullion@pm.me>
2025-02-01 14:56:41 +0000

    Navigation buttons have been styled and aligned.

--- src/components/NavigationButtons.js
+++ src/components/NavigationButtons.js
@@ -1,30 +1,33 @@
 import '../index.css';
+
 export default function NavigationButtons({ currentPage, onNext, onPrevious }) {
   return (
-    <div className="mt-8 flex justify-between px-[15%]">
-      {currentPage > 1 && (
-        <button
-          onClick={onPrevious}
-          className="navigation-btn"
-        >
-          Previous Page
-        </button>
-      )}
-      {currentPage < 4 ? (
-        <button
-          onClick={onNext}
-          className="navigation-btn ml-auto"
-        >
-          Next Page
-        </button>
-      ) : (
-        <button
-          type="submit"
-          className="navigation-btn ml-auto"
-        >
-          Submit Application
-        </button>
-      )}
+    <div className="navigation-container">
+      <div className="navigation-buttons">
+        {currentPage > 1 && (
+          <button
+            onClick={onPrevious}
+            className="navigation-btn"
+          >
+            Previous Page
+          </button>
+        )}
+        {currentPage < 4 ? (
+          <button
+            onClick={onNext}
+            className="navigation-btn"
+          >
+            Next Page
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="navigation-btn"
+          >
+            Submit Application
+          </button>
+        )}
+      </div>
     </div>
   );
 }
