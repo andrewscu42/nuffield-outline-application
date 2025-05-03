2ffbf45d4bc3834a9f42090c21008fd72bea2e45
sandleon <andrew.scullion@pm.me>
2025-05-03 19:48:23 +0100

    changed content of home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'; // Import Link
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to the Research Foundation Application</h1>
+      <h1>Welcome to the Nuffield Foundation Research Development and AnalysisApplication</h1>
       <p>Please click the button below to start your application.</p>
       {/* Use Link to navigate to the /apply route */}
       <Link to="/apply">
